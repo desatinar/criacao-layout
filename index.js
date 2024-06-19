cd5bd04861c9494ed3b1080393dbb7f1b68a8478
Rodrigo Santos <rodrigosdn@gmail.com>
2024-06-19 19:57:37 -0300

    Exercicio Finalizado

---
+++ index.js
@@ -0,0 +1,36 @@
+const dayTasksUl = document.getElementById("day-tasks");
+const nightTasksUl = document.getElementById("night-tasks");
+const dayTasksItems = document.getElementsByClassName("day");
+const nightTasksItems = document.getElementsByClassName("night");
+
+function addTask(){
+    const userInput = document.getElementById("add-task").value;
+    const selectOption = document.getElementById("choice").value;
+
+    if(userInput.trim() === ""){
+        alert("É necessário inserir uma tarefa");
+        return;
+    }
+    const newTaskItem = document.createElement("li");
+    newTaskItem.textContent = userInput;
+    newTaskItem.classList.add("task-list")
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "❌";
+    removeButton.classList.add("remove-task");
+    removeButton.addEventListener("click", function() {
+        newTaskItem.remove();
+    });
+
+    newTaskItem.appendChild(removeButton);
+
+    if(selectOption === "day"){
+        newTaskItem.classList.add("day");
+        dayTasksUl.appendChild(newTaskItem);
+    } else {
+        newTaskItem.classList.add("night");
+        nightTasksUl.appendChild(newTaskItem);
+    }
+
+    document.getElementById("add-task").value = "";
+}
